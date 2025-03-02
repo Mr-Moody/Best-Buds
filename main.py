4eba78ead08857b9837dac1e00d43b56482c3e13
tara-kas <tara@phuketcafe.com>
2025-03-02 00:34:32 +0000

    camera activated! but with issues

--- main.py
+++ main.py
@@ -19,8 +19,11 @@ from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDFillRoundFlatButton
 from kivy.core.text import LabelBase    # for fonts
+from kivy.graphics.texture import Texture   # for camera screen display?
+
 import os
 from PIL import Image   # for image manipulation
+import cv2      # for camera display
 
 LabelBase.register(name="MainFont", fn_regular="static/fonts/EB_Garamond_static/EBGaramond-SemiBold.ttf")
 LabelBase.register(name="SecondaryFont", fn_regular="static/fonts/Comfortaa_static/Comfortaa-Light.ttf")
@@ -39,6 +42,7 @@ COLOURS = {
 Window.size = (720/2, 1280/2)
 Window.resizable = False
 
+
 class CustomImageButton(RelativeLayout):
     image_source = StringProperty("")
     screen_name = StringProperty("")
@@ -52,7 +56,28 @@ class CalendarScreen(Screen):
     pass
 
 class CameraScreen(Screen):
-    pass
+    def on_enter(self):
+        # start the opencv camera feed when entering the screen
+        self.capture = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.update_camera, 1.0/30.0)      # updates every 30FPS
+        
+    def on_leave(self):
+        # stop camera when leaving
+        self.capture.release()
+        Clock.unschedule(self.update_camera)
+    
+    def update_camera(self, dt):
+        # capture frame and display
+        ret, frame = self.capture.read()
+        if ret:
+            frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)    # rotate cam
+            
+            square_size = int(min(self.ids.camera_widget.size))
+            frame_resized = cv2.resize(frame, (square_size, square_size), interpolation=cv2.INTER_AREA)
+            buf = cv2.flip(frame_resized, 0).tobytes()
+            texture = Texture.create(size=(frame_resized.shape[1], frame_resized.shape[0]), colorfmt='bgr')
+            texture.blit_buffer(buf,colorfmt='bgr', bufferfmt='ubyte')
+            self.ids.camera_widget.texture = texture
 
 class SettingsScreen(Screen):
     pass
@@ -139,13 +164,20 @@ class MyApp(MDApp):
         
     def capture_picture(self):
         # take piccy
-        camera = self.root.ids.screen_manager.get_screen("camera").ids.camera_widget
+        camera_screen = self.root.ids.screen_manager.get_screen("camera")
+        
+        #get piccy from opencv
+        ret, frame = camera_screen.capture.read()
+        if not ret:
+            print("no piccy taken :(")
+            return
+        
         # check how many already exist in the folder to number then sequentially
         existing_images = [f for f in os.listdir("images") if f.endswith(".png")]
         next_number = len(existing_images) + 1
         
         filename = f"images/plant_{next_number}.png"
-        camera.export_to_png(filename)  # save image to folder
+        cv2.imwrite(filename, frame)     # save image to folder
         
         if os.path.exists(filename):
             print(f"{filename} picture saved!")
